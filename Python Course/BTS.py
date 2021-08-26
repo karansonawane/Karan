f1572680a97b02fd0580f07d2f5ba2b77054aab6
Karan Sonawane <53928160+karansonawane@users.noreply.github.com>
2021-08-26 12:44:47 +0530

    Python Boolean, Tuples, Sets

---
+++ Python Course/BTS.py
@@ -0,0 +1,33 @@
+from typing import Tuple
+
+
+# Booleans
+
+True
+False
+0
+1
+
+# Tuples
+
+t = (1,2,3)
+print(t[0])
+
+t = ('a', True, 123)
+print(t)
+
+# Sets
+
+x = set()
+
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(4)
+x.add(4)
+x.add(0.12)
+print(x)
+
+
+converted = set([1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4])
+print(converted)
